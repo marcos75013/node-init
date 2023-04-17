96fe792f5dccfb2f9e9484f81c1d90b5d7a46cd9
Marcos Martins <marcos.martins@sncf.fr>
2023-04-17 11:11:19 +0200

    add colors node

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello Jamal!'));
+console.log(chalk.red('Hello Rachid!'));
+console.log(chalk.yellow('Hello Bryce!'));
+console.log(chalk.green('Hello Yannick!'));
